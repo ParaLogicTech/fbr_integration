2ebfc47978cbb64545026297cfbd711b68ba9ef7
Saif Ur Rehman <saif@paralogic.io>
2025-09-05 00:19:22 +0500

    fix: Delete sec_fbr_pos_transaction_details in patch before resetup

--- fbr_integration/patches/resetup_fbr_pos_fields.py
+++ fbr_integration/patches/resetup_fbr_pos_fields.py
@@ -5,5 +5,6 @@ from frappe.utils.fixtures import sync_fixtures
 
 def execute():
 	sync_fixtures("fbr_integration")
+	frappe.delete_doc_if_exists("Custom Field", "Sales Invoice-sec_fbr_pos_transaction_details")
 	if frappe.db.get_single_value("FBR POS Settings", "enable_fbr_pos", cache=False):
 		setup_fbr_pos_fields()
